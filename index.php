86f4893b69b441a82bacc72418e302e33ddb86aa
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-05-15 00:55:22 +0100

    Patched recursive wildcard routing bug

--- index.php
+++ index.php
@@ -7,14 +7,20 @@ use Phlo\Config;
 use Phlo\Core\{MimeType, Router, Rule, RuleType};
 
 
-$loaded_config = Config::load(__DIR__);
-if (!$loaded_config) {
-	die('Failed to load config');
-}
+try {
+	$loaded_config = Config::load(__DIR__);
+	if (!$loaded_config) {
+		die('Failed to load config');
+	}
 
-Router::new()
-	->addRule(Rule::new("api")->setRuleType(RuleType::API)->setAcceptedMimeTypes([MimeType::JSON, MimeType::FORM_URLENCODED])->setTarget("app/api"))
-	->addRule(Rule::new("public")->setRuleType(RuleType::STATIC)->setAcceptedMimeTypes([MimeType::ANY])->setTarget("public"))
-	->addRule(Rule::new("sticky")->setRuleType(RuleType::STICKY)->setTarget("app/pages/index.html"))
-	->addRule(Rule::new("")->setRuleType(RuleType::STATIC)->setAcceptedMimeTypes([MimeType::ANY])->setTarget("app/pages"))
-	->serve();
+	Router::new()
+		->addRule(Rule::new("api")->setRuleType(RuleType::API)->setAcceptedMimeTypes([MimeType::JSON, MimeType::FORM_URLENCODED])->setTarget("app/api"))
+		->addRule(Rule::new("public")->setRuleType(RuleType::STATIC)->setAcceptedMimeTypes([MimeType::ANY])->setTarget("public"))
+		->addRule(Rule::new("sticky")->setRuleType(RuleType::STICKY)->setTarget("app/pages/index.html"))
+		->addRule(Rule::new("")->setRuleType(RuleType::STATIC)->setAcceptedMimeTypes([MimeType::ANY])->setTarget("app/pages"))
+		->serve();
+
+} catch (Exception $e) {
+	// you don't want to show this Exception message in production
+	echo $e->getMessage();
+}
